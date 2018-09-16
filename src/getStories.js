d6e0c8b4274897b14f64ebe87fd3763262181f9f
Amit Zur <sendwithchibo@gmail.com>
2018-09-16 19:09:55 +0300

    add console logs to getStories

--- src/getStories.js
+++ src/getStories.js
@@ -1,20 +1,35 @@
 /* global document */
 function getStories() {
   let menuItems = getAllMenuItems();
+  console.log(`got ${menuItems.length} menu items`);
+
   let closedMenuItems = getClosedMenus(menuItems);
+  console.log(
+    `got ${closedMenuItems.length} closed menu items:\n${menuItemsToString(closedMenuItems)}`,
+  );
 
   while (closedMenuItems.length) {
+    console.log(`opening ${closedMenuItems.length} closed menu items`);
     openMenus(closedMenuItems);
     menuItems = getAllMenuItems();
     closedMenuItems = getClosedMenus(menuItems);
+    console.log(`after opening menus, got ${menuItems.length} menu items`);
+    console.log(
+      `after opening menus, got ${closedMenuItems.length} closed menu items:\n${menuItemsToString(
+        closedMenuItems,
+      )}`,
+    );
   }
 
-  return Array.from(
+  const anchors = Array.from(
     document.querySelectorAll('.Pane.vertical.Pane1 [role="menuitem"] + * a[href]'),
-  ).map(anchor => {
+  );
+  console.log(`returning ${anchors.length} stories.`);
+  return anchors.map((anchor, i) => {
     const url = new URL(anchor.href);
     const kind = decodeURIComponent(url.searchParams.get('selectedKind'));
     const name = decodeURIComponent(url.searchParams.get('selectedStory'));
+    console.log(`${i + 1}) story kind=${kind}, name=${name}`);
     return {
       kind,
       name,
@@ -31,6 +46,10 @@ function getStories() {
 
   function openMenus(menuItems) {
     menuItems.forEach(menuItem => menuItem.click());
+  }
+
+  function menuItemsToString(menuItems) {
+    return menuItems.map(item => item.textContent).join('\n');
   }
 }
 
