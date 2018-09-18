aadd184f2e3738a33d3e65960db751a7a6101a15
Amit Zur <sendwithchibo@gmail.com>
2018-09-18 14:48:13 +0300

    handle navigation timeout in stories

--- src/getStoryData.js
+++ src/getStoryData.js
@@ -1,11 +1,16 @@
 'use strict';
+const {presult} = require('@applitools/functional-commons');
 
 function makeGetStoryData({logger, extractResources, domNodesToCdt}) {
   return async function getStoryData(name, url, page) {
     logger.log(`getting data from story ${name} - ${url}`);
-    await page.goto(url);
+    const [err] = await presult(page.goto(url, {timeout: 10000}));
+    if (err) {
+      logger.log(`error navigating to story ${name}`, err);
+    }
     const {resourceUrls, blobs: resourceContents} = await page.evaluate(extractResources);
     const cdt = await page.evaluate(domNodesToCdt);
+    logger.log(`done getting data from story ${name}`);
     return {name, resourceUrls, resourceContents, cdt, url};
   };
 }
