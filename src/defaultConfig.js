b1eed91dda2c01f88b8ad53fb9427e548a5710b9
Amit Zur <sendwithchibo@gmail.com>
2018-09-25 13:52:43 +0300

    add default storybookConfigDir

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// See README for details
 module.exports = {
   storybookPort: 9000,
   storybookHost: 'localhost',
   startServer: true,
+  storybookConfigDir: '.storybook',
 };
--- test/fixtures/applitools.config.js
+++ test/fixtures/applitools.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  batchName: 'Simple storybook',
   appName: 'Simple storybook',
   storybookConfigDir: 'test/fixtures/appWithStorybook/',
 };
