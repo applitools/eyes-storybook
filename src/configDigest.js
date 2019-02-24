e75b6e438ab5e087bf8c9105be4155f97ed9b85c
Daniel Schwartz <danielsch24@gmail.com>
2019-02-24 16:36:30 +0200

    better print of config in cli startup

--- src/configDigest.js
+++ src/configDigest.js
@@ -1,10 +1,12 @@
 'use strict';
+const {TypeUtils} = require('@applitools/eyes-common');
+const prettyValue = val => (!TypeUtils.isObject(val) ? val : JSON.stringify(val));
 
 function configDigest(config) {
   const maxKeyLen = Object.keys(config).reduce((len, key) => Math.max(len, key.length), 0);
   return (
     Object.entries(config)
-      .map(([key, value]) => `${key.padEnd(maxKeyLen + 1)}: ${value}`)
+      .map(([key, value]) => `${key.padEnd(maxKeyLen + 1)}: ${prettyValue(value)}`)
       .join('\n') + '\n'
   );
 }
