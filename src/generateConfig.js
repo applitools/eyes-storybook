911dc685a4dd15f6acffdf3ab6d8fabad28bfa76
Ben Warbrooke <ben.warbrooke@pushpay.com>
2019-04-23 15:59:20 +1200

    Change waitBeforeScreenshots configuration to use Puppeteer.waitFor()
    rather than delay
    
    Valid values for waitBeforeScreenshots are now: string, positive number or Function

--- src/generateConfig.js
+++ src/generateConfig.js
@@ -10,9 +10,6 @@ function generateConfig({argv = {}, defaultConfig = {}, externalConfigParams = [
   const config = ConfigUtils.getConfig({configPath, configParams});
   const argvConfig = pick(argv, configParams);
   const result = Object.assign({}, defaultConfig, config, argvConfig);
-  if (result.waitBeforeScreenshots) {
-    result.waitBeforeScreenshots = Number(result.waitBeforeScreenshots);
-  }
   return result;
 }
 
