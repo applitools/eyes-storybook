9ff6108fc8775ef357149df815c08a8396e376d5
Daniel Schwartz <danielsch24@gmail.com>
2019-09-23 11:29:22 +0300

    changed concurrency message

--- src/concurrencyMsg.js
+++ src/concurrencyMsg.js
@@ -1,11 +1,9 @@
 'use strict';
 const chalk = require('chalk');
 
-module.exports = chalk.yellow(
-  `
-Important notice: the Applitools visual tests are currently running with a concurrency value of 10.
-This means that only up to 10 visual tests can run in parallel, and therefore the execution might be slow. This is the default behavior for free accounts.
-If your account does support a higher level of concurrency, it's possible to pass a different value by specifying \`concurrency:X\` in the applitools.config.js file.
-For more information on how to configure the concurrency level, visit the following link: https://www.npmjs.com/package/@applitools/eyes-storybook#concurrency.
-If you are interested in speeding up your visual tests, contact sdr@applitools.com to get a trial account and a higher level of concurrency.`,
-);
+module.exports = chalk.yellow(`
+Important notice: Your Applitools visual tests are currently running with a concurrency value of 10.
+This means that only up to 10 visual tests can run in parallel, and therefore the execution might be slower.
+If your Applitools license supports a higher concurrency level, learn how to configure it here: https://www.npmjs.com/package/@applitools/eyes-storybook#concurrency.
+Need a higher concurrency in your account? Email us @ sdr@applitools.com with your required concurrency level.
+`);
