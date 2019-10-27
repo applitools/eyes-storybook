5b5d80f096b3bb6d989c3ee035ef82621037d4bd
Amit Zur <sendwithchibo@gmail.com>
2019-10-27 14:56:33 +0200

    configured eyes-storybook task

---
+++ scripts/preconfigured.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  concurrency: 100,
+  showLogs: true,
+  appName: process.env.APPLITOOLS_APP_NAME || 'Test storybook',
+  batchName: process.env.APPLITOOLS_APP_NAME || 'Test storybook',
+  storybookUrl: process.env.APPLITOOLS_STORYBOOK_URL,
+  readStoriesTimeout: 300000,
+  waitBeforeScreenshots: 200,
+  browser: [
+    {width: 1200, height: 800, name: 'chrome'},
+    {width: 800, height: 600, name: 'chrome'},
+    {width: 1200, height: 800, name: 'firefox'},
+    {width: 800, height: 600, name: 'firefox'},
+    {deviceName: 'iPhone X'},
+    // {width: 1200, height: 800, name: 'ie11'},
+    // {width: 800, height: 600, name: 'ie11'},
+    // {width: 1200, height: 800, name: 'edge'},
+    // {width: 800, height: 600, name: 'edge'},
+  ],
+};
