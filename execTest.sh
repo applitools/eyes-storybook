e06506c8acb6fb1e706ecf9267b5088c7bec06a7
Yarden.Ingber <yarden.ingber@applitools.com>
2019-11-07 23:00:13 +0200

    print test to travis log

--- execTest.sh
+++ execTest.sh
@@ -1,2 +1,2 @@
 npm install
-APPLITOOLS_API_KEY=wPVBepqUxtQg50EnvQYiyLa7FQx6qA2bXSASZqWfUQI110 APPLITOOLS_SERVER_URL=https://atlassianeyesapi.applitools.com APPLITOOLS_BATCH_NAME=Yarden_24_7_tests APPLITOOLS_APP_NAME=Jira APPLITOOLS_SHOW_LOGS=true APPLITOOLS_STORYBOOK_URL=https://jira-storybook.staging.atl-paas.net/?path=/story/* npm run eyes-storybook:configured > yarden_tests_chrome_firefox.log
+APPLITOOLS_API_KEY=wPVBepqUxtQg50EnvQYiyLa7FQx6qA2bXSASZqWfUQI110 APPLITOOLS_SERVER_URL=https://atlassianeyesapi.applitools.com APPLITOOLS_BATCH_NAME=Yarden_24_7_tests APPLITOOLS_APP_NAME=Jira APPLITOOLS_SHOW_LOGS=true APPLITOOLS_STORYBOOK_URL=https://jira-storybook.staging.atl-paas.net/?path=/story/* npm run eyes-storybook:configured
