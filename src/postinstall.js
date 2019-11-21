d4ad85c25e591199872055f3d181e498d52e70ae
Amit Zur <sendwithchibo@gmail.com>
2019-11-21 14:28:29 +0200

    postinstall message

--- src/postinstall.js
+++ src/postinstall.js
@@ -5,9 +5,12 @@ console.log(
     `This is version 3 of ${chalk.cyan('@applitools/eyes-storybook')}.
 This version includes a wide variety of performance improvements and features.
 We recommend checking out the release notes:
-${chalk.yellow(
-  'https://github.com/applitools/eyes-storybook/blob/v3.0.0/docs/release-notes-v3.md',
-)}`,
+${chalk.yellow('https://github.com/applitools/eyes-storybook/blob/v3.0.1/docs/release-notes-v3.md')}
+
+If you experience a difference in behavior between v2 and v3, it's possible to opt out of the optimization
+by configuring ${chalk.cyan('reloadTabPerStory')} to ${chalk.cyan(
+      'true',
+    )}. And, do read the release notes - they explain it perfectly :-)`,
     {padding: 1, borderColor: 'cyan', align: 'center'},
   ),
 );
